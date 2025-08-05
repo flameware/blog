540f629ddeb4ea96ffc172d50030d0ec0ccde4e8
Seongki Sohn <flameware@gmail.com>
2025-08-05 23:05:17 +0900

    Initial commit: Setup Astro blog by using Storyteller theme

--- src/site.config.mjs
+++ src/site.config.mjs
@@ -1,34 +1,34 @@
 export default {
-    siteTitle: "The Storyteller", // Main site title displayed in header
-    siteSubTitle: "Minimal musings on code, design, and life", // Subtitle shown under main title
-    copyright: "© 2025 The Storyteller. All Rights Reserved.", // Footer copyright text
+    siteTitle: "브을로그", // Main site title displayed in header
+    siteSubTitle: "좋아하는 것들에 대한 끄적거림", // Subtitle shown under main title
+    copyright: "© 2025 Seongki Sohn. All Rights Reserved.", // Footer copyright text
     showAuthorsOnHomePage: false, // Display author info on homepage
     showFeaturrdPostsOnHomePage: true, // Show featured posts section on homepage
     showCategoryOnPosts: true, // Display categories on posts in homepage
     labels: {
-        featuredPosts: "Featured Posts", // Title for featured posts section
-        latestPosts: "Latest Posts", // Title for latest posts section
-        viewAllPosts: "View All Posts", // Text for link to paginated blog
-        backToHome: "Back to Home", // Back navigation text
-        youMightAlsoLike: "You Might Also Like", // Similar posts section title
+        featuredPosts: "추천 글", // Title for featured posts section
+        latestPosts: "최근 글", // Title for latest posts section
+        viewAllPosts: "모두 보기", // Text for link to paginated blog
+        backToHome: "돌아가기", // Back navigation text
+        youMightAlsoLike: "이런건 어떠세요", // Similar posts section title
         postedIn: "Posted in", // Category prefix text in articles
-        noArticlesFound: "No articles found.", // Empty state message
-        allCategories: "All Categories", // Categories page title
-        allTags: "All Tags", // Tags page title
-        allAuthors: "All Authors", // Authors page title
-        exploreArticlesByTags: "Explore articles organized by topics", // Tags page description
-        exploreArticlesByCategories: "Explore articles organized by topics", // Categories page description
-        exploreArticlesByAuthors: "Explore articles organized by authors", // Authors page description
-        readMore: "Read More", // Text for "Read More" links on featured posts
-        shareThisArticle: "Share this article", // Share button text
+        noArticlesFound: "글이 없습니다.", // Empty state message
+        allCategories: "모든 카테고리", // Categories page title
+        allTags: "모든 태그", // Tags page title
+        allAuthors: "모든 작성자", // Authors page title
+        exploreArticlesByTags: "태그에 따라 찾아보기", // Tags page description
+        exploreArticlesByCategories: "카테고리에 따라 찾아보기", // Categories page description
+        exploreArticlesByAuthors: "작성자에 따라 찾아보기", // Authors page description
+        readMore: "더 보기", // Text for "Read More" links on featured posts
+        shareThisArticle: "공유하기", // Share button text
     },
     pagination: {
         showPrevNext: true, // Show Previous/Next navigation buttons
-        prevText: "Previous", // Text for previous page button
-        nextText: "Next", // Text for next page button
-        postLabel: "posts", // Label used in pagination info (e.g., "8 posts")
+        prevText: "이전", // Text for previous page button
+        nextText: "다음", // Text for next page button
+        postLabel: "글", // Label used in pagination info (e.g., "8 posts")
     },
-    defaultAuthorName: "Hasin Hayder",
+    defaultAuthorName: "Seongki Sohn",
     showCategoriesLinkOnFooter: true, // Show Categories link in footer
     showTagsLinkOnFooter: true, // Show Tags link in footer
     showAuthorsLinkOnFooter: true, // Show Authors link in footer
@@ -38,6 +38,6 @@ export default {
     numberOfLatestPostsOnHomePage: 6, // Number of latest posts on homepage
     numberOfBlogPostsPerPage: 8, // Number of posts per paginated blog
 
-    gTag: "G-V5QHDKBFP" // Google Analytics tracking ID
+    //gTag: "G-V5QHDKBFP" // Google Analytics tracking ID
 }
 
